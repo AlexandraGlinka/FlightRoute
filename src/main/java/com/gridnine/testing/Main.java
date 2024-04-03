e386827ee19392a7ff6a8f9d533826be82c8584a
glinka <alexandraglinka@yandex.ru>
2024-04-03 20:24:44 +0300

    обработан метод filterBeforeNow

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -1,6 +1,8 @@
 package com.gridnine.testing;
 
 import com.gridnine.testing.entity.Flight;
+import com.gridnine.testing.service.FlightFilter;
+import com.gridnine.testing.service.FlightFilterImpl;
 import com.gridnine.testing.testingData.FlightBuilder;
 
 import java.util.List;
@@ -9,8 +11,19 @@ public class Main {
     public static void main(String[] args) {
         List<Flight> flights = FlightBuilder.createFlights();
 
-        for (Flight flight : flights) {
-            System.out.println(flight);
-        }
+        System.out.println("Тестовые данные:");
+        flights.forEach(System.out::println);
+        System.out.println(separator());
+
+        System.out.println("Правило исключения 1: Вылет до текущего момента времени.");
+        List<Flight> filteredFlights = new FlightFilterImpl().filterBeforeNow(flights);
+        filteredFlights.forEach(System.out::println);
+        System.out.println(separator());
+
+        System.out.println("Правило исключения 2: Сегменты с датой прилёта раньше даты вылета.");
+    }
+
+    public static String separator() {
+        return "----------------------";
     }
 }
--- src/main/java/com/gridnine/testing/service/FlightFilterImpl.java
+++ src/main/java/com/gridnine/testing/service/FlightFilterImpl.java
@@ -3,12 +3,17 @@ package com.gridnine.testing.service;
 import com.gridnine.testing.entity.Flight;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FlightFilterImpl implements FlightFilter {
 
     @Override
-    public List<Flight> filterBeforeNow() {
-        return null;
+    public List<Flight> filterBeforeNow(List<Flight> flights) {
+        List<Flight> filteredFlights = flights.stream()
+                .filter(flight -> flight.getSegments().stream()
+                        .allMatch(segment -> segment.getDepartureDate().isBefore(segment.getArrivalDate())))
+                .collect(Collectors.toList());
+        return filteredFlights;
     }
 
     @Override
